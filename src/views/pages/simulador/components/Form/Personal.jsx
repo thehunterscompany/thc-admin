ca945fc1c8fcf3a0ce5d220647820f3086816644
Armando Zubillaga <ajzpiv97@gmail.com>
2021-08-07 15:20:10 -0400

    TA-21 Created personal form

---
+++ src/views/pages/simulador/components/Form/Personal.jsx
@@ -0,0 +1,182 @@
+import React, { useState, useMemo } from 'react'
+import PropTypes from 'prop-types'
+import { Grid } from '@material-ui/core'
+import { InputField, SelectField, DateField } from '../../../../../components/FormFields'
+import { FormControl, FormControlLabel, Switch } from '@material-ui/core'
+import countryList from 'react-select-country-list'
+
+const simulationOptions = [
+  { value: 1, label: 'Calular Cuota' },
+  { value: 2, label: 'Cuanto me prestan' },
+]
+
+const rateSimulation = [
+  { value: 1, label: 'Financiación de vivienda' },
+  { value: 2, label: 'Financiación inmuebles comerciales' },
+  { value: 3, label: 'Compra de cartera' },
+]
+
+const idType = [
+  { value: 1, label: 'Cedula de ciudadanía' },
+  { value: 2, label: 'Cedula de extrangería' },
+  { value: 3, label: 'Pasaporte' },
+]
+
+const PersonalFields = ({ formField, values }) => {
+  const [state, setState] = useState({
+    checkedA: false,
+    checkedB: false,
+  })
+
+  const {
+    firstNames,
+    lastNames,
+    documentType,
+    documentId,
+    dateOfBirth,
+    email,
+    countryCode,
+    telephone,
+    simulation,
+    simulationType,
+  } = formField
+
+  const options = useMemo(() => countryList().getData(), [])
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked })
+  }
+
+  const calculateMaxDate = () => {
+    let today = new Date()
+    let year = today.getFullYear()
+    let yearDiff = year - 18
+    today.setFullYear(yearDiff)
+    return today
+  }
+
+  return (
+    <React.Fragment>
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={6}>
+          <InputField name={firstNames.name} label={firstNames.label} type="text" fullWidth />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <InputField name={lastNames.name} label={lastNames.label} type="text" fullWidth />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <SelectField
+            name={documentType.name}
+            label={documentType.label}
+            data={idType}
+            fullWidth
+            style={{ marginTop: '25px' }}
+          />
+        </Grid>
+
+        <Grid item xs={12} md={6}>
+          <InputField name={documentId.name} label={documentId.label} type="text" fullWidth />
+        </Grid>
+
+        <Grid item xs={12} md={6}>
+          <DateField
+            name={dateOfBirth.name}
+            label={dateOfBirth.label}
+            format="dd/MM/yyyy"
+            minDate={new Date('1900/01/01')}
+            maxDate={calculateMaxDate()}
+            style={{ marginTop: '25px' }}
+            fullWidth
+          />
+        </Grid>
+
+        <Grid item xs={12} md={6}>
+          <InputField name={email.name} label={email.label} type="text" fullWidth />
+        </Grid>
+
+        <Grid item xs={12} md={6}>
+          <SelectField
+            name={countryCode.name}
+            label={countryCode.label}
+            data={options}
+            fullWidth
+            style={{ marginTop: '25px' }}
+          />
+        </Grid>
+
+        <Grid item xs={12} md={6}>
+          <InputField name={telephone.name} label={telephone.label} type="text" fullWidth />
+        </Grid>
+
+        <Grid item xs={12} md={values.simulation === 1 ? 6 : 12}>
+          <SelectField
+            name={simulation.name}
+            label={simulation.label}
+            data={simulationOptions}
+            fullWidth
+          />
+        </Grid>
+
+        {values.simulation === 1 ? (
+          <Grid item xs={12} md={6}>
+            <SelectField
+              name={simulationType.name}
+              label={simulationType.label}
+              data={rateSimulation}
+              fullWidth
+            />
+          </Grid>
+        ) : null}
+
+        <Grid item xs={12}>
+          <FormControl>
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={state.checkedA}
+                  onChange={handleChange}
+                  name="checkedA"
+                  color="primary"
+                />
+              }
+            />
+          </FormControl>
+
+          <small>
+            Acepto{' '}
+            <a
+              href="https://thcsas.com.co/terminos-y-condiciones"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              términos, condiciones y política de tratamiento de datos
+            </a>
+          </small>
+        </Grid>
+
+        <Grid item xs={12}>
+          <FormControl>
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={state.checkedB}
+                  onChange={handleChange}
+                  name="checkedB"
+                  color="primary"
+                />
+              }
+            />
+          </FormControl>
+          <small>Autorizo a thcsas.com.co a consultar mi información en centrales de riesgo.</small>
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  )
+}
+
+PersonalFields.propTypes = {
+  formField: PropTypes.object,
+  values: PropTypes.object,
+}
+
+export default PersonalFields
---
+++ src/views/pages/simulador/components/Form/index.jsx
@@ -0,0 +1,105 @@
+import React, { useState } from 'react'
+import { Formik, Form } from 'formik'
+import { CButton } from '@coreui/react'
+import { Stepper, Step, StepLabel, Button, CircularProgress } from '@material-ui/core'
+import FinancialFields from './Financial'
+import PersonalFields from './Personal'
+import OperationalFields from './Operación'
+
+import useStyles from './style'
+import formFields from '../FormModel/simulationFormModel'
+
+const steps = ['Datos Personales', 'Datos Económicos', 'Datos de la operación']
+
+const renderStepForms = (step, values) => {
+  if (step === 0) return <PersonalFields formField={formField.personal} values={values} />
+  if (step === 1) return <FinancialFields formField={formField.financial} values={values} />
+  if (step === 2 && values?.simulation === 2) return null
+  if (step === 2) return <OperationalFields formField={formField.operational} values={values} />
+  // case 2:
+  //   return <OperationalFields formField={formField.operational} values={values} />
+  // case 3:
+  //   return null
+}
+
+const { formId, formField } = formFields
+
+const SimulatorForm = () => {
+  const classes = useStyles()
+  const [activeStep, setActiveStep] = useState(0)
+  const isLastStep = activeStep === steps.length - 1
+
+  function _handleSubmit(values, actions) {
+    if (isLastStep) {
+      // _submitForm(values, actions);
+      console.log('hi')
+    } else {
+      setActiveStep(activeStep + 1)
+      actions.setTouched({})
+      actions.setSubmitting(false)
+    }
+  }
+
+  function _handleBack() {
+    setActiveStep(activeStep - 1)
+  }
+
+  return (
+    <React.Fragment>
+      <Stepper activeStep={activeStep} alternativeLabel className={classes.stepper}>
+        {steps.map((label) => (
+          <Step key={label}>
+            <StepLabel className={classes.svg}>{label}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+      <>
+        <Formik
+          initialValues={{
+            checkedA: false,
+            checkedB: false,
+            dateOfBirth: '',
+            tenants: [],
+          }}
+          onSubmit={(values, actions) => {
+            // props.postParams(values, resetForm)
+            _handleSubmit(values, actions)
+          }}
+          validationSchema={''}
+        >
+          {({ handleChange, isSubmitting, values }) => {
+            console.log(values)
+            return (
+              <Form onChange={handleChange} id={formId}>
+                {renderStepForms(activeStep, values)}
+                <div className={classes.buttons}>
+                  {activeStep !== 0 && (
+                    <Button onClick={_handleBack} className={classes.button}>
+                      Regresar
+                    </Button>
+                  )}
+                  <div className={classes.wrapper}>
+                    <Button
+                      disabled={isSubmitting || !values.checkedA || !values.checkedB}
+                      type="submit"
+                      variant="contained"
+                      color="primary"
+                      className={classes.button}
+                    >
+                      {isLastStep ? 'Simular' : 'Continuar'}
+                    </Button>
+                    {isSubmitting && (
+                      <CircularProgress size={24} className={classes.buttonProgress} />
+                    )}
+                  </div>
+                </div>
+              </Form>
+            )
+          }}
+        </Formik>
+      </>
+    </React.Fragment>
+  )
+}
+
+export default SimulatorForm
