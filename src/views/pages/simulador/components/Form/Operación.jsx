ec5fa63d8af2ce63734a70eb475060f33ec9be6c
Armando Zubillaga <ajzpiv97@gmail.com>
2021-08-07 15:20:28 -0400

    TA-23 Created operation form

---
+++ src/views/pages/simulador/components/Form/Operación.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useMemo, useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { Grid } from '@material-ui/core'
+import { InputField, SelectField } from '../../../../../components/FormFields'
+import { FormControl, FormControlLabel, Button } from '@material-ui/core'
+import country from 'currency-codes'
+
+const OperationalFields = ({ formField, values }) => {
+  return (
+    <div>
+      <h3>Hi</h3>
+    </div>
+  )
+}
+
+OperationalFields.propTypes = {
+  formField: PropTypes.object,
+  values: PropTypes.object,
+}
+
+export default OperationalFields
