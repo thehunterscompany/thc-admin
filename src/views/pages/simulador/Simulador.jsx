38ad429b740efd340027819b6a42e55809fed6f4
Armando Zubillaga <ajzpiv97@gmail.com>
2021-08-07 15:19:34 -0400

    TA-24 Created simulador page

--- src/views/pages/simulador/Simulador.jsx
+++ src/views/pages/simulador/Simulador.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import './Simulador.scss'
 import Navbar from '../../../components/Navbar/Navbar'
+import Simulation from './components/Simulation'
+import SimulatorForm from './components/Form'
 
 const Simulador = () => (
-  <div style={{ background: 'white' }}>
+  <div className="_simulador-container">
     <Navbar />
-    <div className="_form-spaces">
-      <h1>Hello</h1>
+    <div>
+      <div className="_form-spaces">
+        <h1>Para darte la mejor opción necesitamos algunos datos</h1>
+        <Simulation />
+      </div>
     </div>
   </div>
 )
